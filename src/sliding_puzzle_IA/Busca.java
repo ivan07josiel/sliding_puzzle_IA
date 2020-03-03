2008cd0f9d01269448a9a7c30684ae672a59954f
Ivan Josiel <ivan07josiel@gmail.com>
2020-03-03 11:45:43 -0300

    Refatorando classe de busca

--- src/sliding_puzzle_IA/Busca.java
+++ src/sliding_puzzle_IA/Busca.java
@@ -6,10 +6,14 @@ import java.util.List;
 import movimentos.Movimento;
 
 public class Busca {
-	private Estado estadofinal;
+	private Estado estadoFinal;
 	private No solucao;
 	private boolean hasSolucao = false;
-	private List<No> nos;
+	private List<No> nos = new ArrayList<>();
+
+	public Busca(Estado estadoFinal) {
+		this.estadoFinal = estadoFinal;
+	}
 	
 	public void realizarBusca(Estado estadoInicial) {
 		
@@ -21,21 +25,21 @@ public class Busca {
 	}
 	
 	private No getNoInicial(Estado estadoInicial) {
-		Estado antecessor = new Estado(null, new Peca[0][0], null);
+		Estado antecessor = new Estado(null, new Peca[3][3], null);
 		return new No(antecessor, estadoInicial, 0);
 	}
 
 	public void buscar(No no) {
 		if (hasSolucao) {
 			if (no.estado.distanciaTotal < solucao.distanciaTotal) {
-				if (no.estado.equals(estadofinal)) {
+				if (no.estado.isEstadoMeta()) {
 					solucao = no;
 				} else {
 					expandirNos(no);
 				}
 			}
 		} else {
-			if (no.estado.equals(estadofinal)) {
+			if (no.estado.isEstadoMeta()) {
 				solucao = no;
 			} else {
 				expandirNos(no);
