0d34522b1aba659fac239e09c5012d7dc39d8085
Ivan Josiel <ivan07josiel@gmail.com>
2020-03-03 11:44:32 -0300

    Refatorando classe de estado

--- src/sliding_puzzle_IA/Estado.java
+++ src/sliding_puzzle_IA/Estado.java
@@ -2,6 +2,7 @@ package sliding_puzzle_IA;
 
 /**
  * Classe que representa um estado do jogo
+ * 
  * @author Ivan Josiel
  *
  */
@@ -10,45 +11,45 @@ public class Estado implements Cloneable {
 	public Acao acao;
 	public Peca pecas[][] = new Peca[3][3];
 	public Peca posicaoVazia;
-	
-	
+
 	public Estado(Acao acao, Peca[][] pecas, Peca posicaoVazia) {
-		this.distanciaTotal = getDistanciaTotal();
 		this.acao = acao;
 		this.pecas = pecas;
 		this.posicaoVazia = posicaoVazia;
+		this.distanciaTotal = getDistanciaTotal();
 	}
 
 	/**
-	 * Retorna a soma das distancias entre posição atual e posição final de cada peça
+	 * Retorna a soma das distancias entre posição atual e posição final de cada
+	 * peça
 	 */
 	public int getDistanciaTotal() {
 		distanciaTotal = 0;
 
 		for (int i = 0; i < pecas.length; i++) {
-			for (Peca peca : pecas[i]) {
-				distanciaTotal += peca.getDistancia();
+			for (int j = 0; j < pecas[i].length; j++) {
+				Peca peca = pecas[i][j];
+				distanciaTotal += peca != null ? peca.getDistancia(j, i) : 0;
 			}
 		}
-		
+
 		return distanciaTotal;
 	}
-	
+
 	/**
 	 * Indica se o estado atual é igual ao estado objetivo
 	 */
 	public boolean isEstadoMeta() {
 		return getDistanciaTotal() == 0;
 	}
-	
-	public Estado clone(){
-        try {
+
+	public Estado clone() {
+		try {
 			return (Estado) super.clone();
 		} catch (CloneNotSupportedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        return null;
- }
+
+		return null;
+	}
 }
